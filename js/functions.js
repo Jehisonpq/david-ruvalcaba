c04d9d9a4bb302deddfb29f1d866c59e90d9d33a
Jehison <jehisonpq@gmail.com>
2019-11-15 20:26:01 -0600

    estilos contacto y footer desktop

--- js/functions.js
+++ js/functions.js
@@ -23,5 +23,126 @@ $(document).ready(function(){
                 loop:false
             }
         }
-    })
+    });
+    initMap();
 });
+
+function initMap() {
+    var customMapType = new google.maps.StyledMapType([
+      {
+          "featureType": "landscape",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "stylers": [
+              {
+                  "hue": "#00aaff"
+              },
+              {
+                  "saturation": -100
+              },
+              {
+                  "gamma": 2.15
+              },
+              {
+                  "lightness": 12
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "lightness": 24
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 57
+              }
+          ]
+      }
+  ], {
+        name: 'Custom Style'
+    });
+    var customMapTypeId = 'custom_style';
+  
+  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: 20.6778102, lng: -103.3752161},
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+      }
+    });
+  
+    map.mapTypes.set(customMapTypeId, customMapType);
+    map.setMapTypeId(customMapTypeId);
+  
+    var image = 'images/pinMapa.png';
+    var marker = new google.maps.Marker({
+      position: {lat: 20.6778102, lng: -103.3752161	},
+      map: map,
+      icon: image
+    });
+  
+    map.addListener('center_changed', function() {
+      window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+      }, 3000);
+    });
+  
+    marker.addListener('click', function() {
+      map.setZoom(16);
+      map.setCenter(marker.getPosition());
+    });
+  }
